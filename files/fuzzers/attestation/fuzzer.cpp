2f48f0c2991f37ea7c5b39c08f5d89fe2409bd2a
Guido Vranken <guidovranken@gmail.com>
2019-05-30 01:28:20 +0200

    Add fuzzers for block_header, attestation, transfer, voluntary_exit, attester_slashing

---
+++ files/fuzzers/attestation/fuzzer.cpp
@@ -0,0 +1,26 @@
+#define GO_FUZZ_PREFIX attestation_
+#include <lib/differential.h>
+#include <lib/go.h>
+#include <cstring>
+
+std::shared_ptr<fuzzing::Go> go = nullptr;
+
+std::unique_ptr<fuzzing::Differential> differential = nullptr;
+
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
+    differential = std::make_unique<fuzzing::Differential>();
+
+    differential->AddModule(
+            go = std::make_shared<fuzzing::Go>()
+    );
+
+    return 0;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    std::vector<uint8_t> v(data, data + size);
+
+    differential->Run(v);
+
+    return 0;
+}
---
+++ files/fuzzers/block_header/go/fuzz.go
@@ -0,0 +1,35 @@
+package fuzz
+
+import (
+	"github.com/protolambda/zrnt/eth2/beacon"
+	"github.com/protolambda/zrnt/eth2/beacon/block_processing"
+    go_ssz "github.com/prysmaticlabs/go-ssz"
+    "bytes"
+    "bufio"
+)
+
+type DepositTestCase struct {
+	Pre         beacon.BeaconState
+	Block       beacon.BeaconBlock
+}
+
+func Fuzz(data []byte) []byte {
+    reader := bytes.NewReader(data)
+
+    var input DepositTestCase;
+    if err := go_ssz.Decode(reader, &input); err != nil {
+        return []byte{}
+    }
+
+    if err := block_processing.ProcessBlockHeader(&input.Pre, &input.Block); err != nil {
+        return []byte{}
+    }
+
+    var ret bytes.Buffer
+    writer := bufio.NewWriter(&ret)
+    if err := go_ssz.Encode(writer, input.Pre); err != nil {
+        return []byte{}
+    }
+
+    return ret.Bytes()
+}

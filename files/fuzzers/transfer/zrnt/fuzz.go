39e262652722a88b78402c84d7c6e338aa8676be
Guido Vranken <guidovranken@gmail.com>
2019-06-02 01:31:41 +0200

    Transfer fuzzer: implement SSZ preprocessing

--- files/fuzzers/transfer/zrnt/fuzz.go
+++ files/fuzzers/transfer/zrnt/fuzz.go
@@ -1,5 +1,7 @@
 package fuzz
 
+import "C"
+
 import (
 	"github.com/protolambda/zrnt/eth2/beacon"
 	"github.com/protolambda/zrnt/eth2/beacon/block_processing"
@@ -32,4 +34,49 @@ func Fuzz(data []byte) []byte {
     }
 
     return ret.Bytes()
+}
+
+var g_return_data = make([]byte, 0);
+
+//export SSZPreprocessGetReturnData
+func SSZPreprocessGetReturnData(return_data []byte) {
+    copy(return_data, g_return_data)
+}
+
+//export SSZPreprocess
+func SSZPreprocess(data []byte) int {
+    g_return_data = []byte{}
+    var input Input;
+
+    /* Decode */
+    {
+        reader := bytes.NewReader(data)
+        if err := go_ssz.Decode(reader, &input); err != nil {
+            return 0
+        }
+    }
+
+    /* Modify */
+
+    /* Invariant */
+    {
+        if len(input.Pre.Balances) != len(input.Pre.ValidatorRegistry) {
+            return 0
+        }
+    }
+
+    var ret bytes.Buffer
+
+    /* Encode */
+    {
+        writer := bufio.NewWriter(&ret)
+        if err := go_ssz.Encode(writer, input); err != nil {
+            g_return_data = []byte{}
+            return len(g_return_data)
+        }
+    }
+
+    /* Return modified, SSZ-encoded array */
+    g_return_data = ret.Bytes()
+    return len(g_return_data)
 }

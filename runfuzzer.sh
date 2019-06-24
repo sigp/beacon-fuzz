cb685083b899aa51009095adb0927f40a74c2c95
Guido Vranken <guidovranken@gmail.com>
2019-06-24 23:35:25 +0200

    runfuzzer.sh: user-provided fuzzer corpus and state corpus

--- runfuzzer.sh
+++ runfuzzer.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ]
+if [ "$#" -lt 3 ]
 then
-    echo "Specify the fuzzer you want to run"
+    echo "Specify the fuzzer you want to run, the fuzzer corpus directory, and the state corpus directory"
     exit 1
 fi
+
+FUZZER_CORPUS=$(realpath $2)
+STATE_CORPUS_PATH=$(realpath $3)
 
 docker build . -t eth2-fuzzers
 
 mkdir -p corpora
 
-docker run -v `realpath corpora`:/eth2/corpora -t eth2-fuzzers /bin/sh -c "export ETH2_FUZZER_STATE_CORPUS_PATH=/eth2/fuzzers/state-corpus && mkdir -p /eth2/corpora/$1 && /eth2/fuzzers/$1/fuzzer /eth2/corpora/$1 $2"
+docker run \
+    -v $FUZZER_CORPUS:/eth2/corpus \
+    -v $STATE_CORPUS_PATH:/eth2/state-corpus \
+    -t eth2-fuzzers /bin/sh -c \
+        "export ETH2_FUZZER_STATE_CORPUS_PATH=/eth2/state-corpus && /eth2/fuzzers/$1/fuzzer /eth2/corpus $4"

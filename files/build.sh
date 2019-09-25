52f405314985c22d554464c74210b60d7fd09ef2
gnattishness <1620192+gnattishness@users.noreply.github.com>
2019-09-25 17:10:39 +1000

    build.sh to avoid hard-coding zrnt dependencies.
    Restructured to separate zrnt go dependencies from go-fuzz ones.
    
    Builds successfully, still some runtime testing to do.

--- files/build.sh
+++ files/build.sh
@@ -8,46 +8,10 @@ cd /eth2
 export CC=clang-6.0
 export CXX=clang++-6.0
 
-# Set env variables for using Golang
-export GOROOT=`realpath go`
-export GOPATH=$GOROOT/packages
-mkdir $GOPATH
-export PATH=$GOROOT/bin:$PATH
-export PATH=$GOROOT/packages/bin:$PATH
-
-# Get custom go-fuzz
-mkdir -p $GOPATH/src/github.com/dvyukov
-cd $GOPATH/src/github.com/dvyukov
-git clone https://github.com/guidovranken/go-fuzz.git
-cd go-fuzz
-git checkout libfuzzer-extensions
-
-mkdir -p $GOPATH/src/github.com/protolambda
-
-cd $GOPATH/src/github.com/protolambda
-git clone https://github.com/ethereum/eth2.0-specs
-cd eth2.0-specs
-git checkout v0.6.0
-
-# Get and configure zrnt
-mkdir -p $GOPATH/src/github.com/protolambda
-cd $GOPATH/src/github.com/protolambda
-git clone https://github.com/protolambda/zrnt.git
-cd zrnt
-git fetch origin v07x:v07x
-git checkout v07x
-go get github.com/protolambda/zssz
-go get github.com/cespare/xxhash
-cd /eth2
-
 # Get eth2.0-specs
 git clone --depth 1 https://github.com/ethereum/eth2.0-specs.git
 export ETH2_SPECS_PATH=`realpath eth2.0-specs/`
 cd /eth2
-
-go get golang.org/x/tools/go/packages
-go build github.com/dvyukov/go-fuzz/go-fuzz-build
-export GO_FUZZ_BUILD_PATH=`realpath go-fuzz-build`
 
 # CPython
 mkdir cpython-install
@@ -58,9 +22,78 @@ make -j$(nproc)
 make install
 
 cd /eth2/lib
+# NOTE this doesn't depend on any GOPATH
 make
+cd /eth2
 
-export GOPATH=$GOPATH:/eth2/lib/go
+# Set env variables for using Golang
+export GOROOT=`realpath go`
+export PATH="$GOROOT/bin:$PATH"
+export GO111MODULE="off" # not supported by go-fuzz, keep it off unless explicitly enabled
+
+# Get and configure zrnt
+ZRNT_GOPATH="/eth2/zrnt_gopath/"
+ZRNT_TMP="/eth2/zrnt_tmp/"
+# TODO choose to error or remove if these paths already exist?
+rm -rf "$ZRNT_GOPATH"
+rm -rf "$ZRNT_TMP"
+mkdir -p "$ZRNT_TMP"
+cd $ZRNT_TMP
+git clone https://github.com/protolambda/zrnt.git
+cd zrnt
+# TODO variables for relevant branch
+git fetch origin v07x:v07x
+git checkout v07x
+# hacky way to use module dependencies with go fuzz
+# see https://github.com/dvyukov/go-fuzz/issues/195#issuecomment-523526736
+# TODO avoid a single GOPATH passed everywhere
+# TODO have a zrnt go dependency section
+# turn GO111MODULE on in case it was set off or auto in go v1.12
+GO111MODULE="on" go mod vendor
+mkdir -p "$ZRNT_GOPATH/src/"
+# TODO does this copy the file or only the directories? i.e. does */ do any different to *?
+mv vendor/*/ "$ZRNT_GOPATH/src/"
+rm -rf vendor
+mkdir -p $ZRNT_GOPATH/src/github.com/protolambda
+cd ..
+mv zrnt "$ZRNT_GOPATH/src/github.com/protolambda/"
+
+cd /eth2
+rm -rf $ZRNT_TMP
+# Now ZRNT_GOPATH contains (only) zrnt and all its dependencies.
+
+export GOPATH="$GOROOT/packages"
+mkdir $GOPATH
+export PATH="$GOPATH/bin:$PATH"
+
+# Get custom go-fuzz
+mkdir -p $GOPATH/src/github.com/dvyukov
+cd $GOPATH/src/github.com/dvyukov
+git clone https://github.com/guidovranken/go-fuzz.git
+cd go-fuzz
+git checkout libfuzzer-extensions
+
+cd /eth2
+
+# TODO should this be in a ZRNT specific spot or common fuzzer?
+# common $GOPATH for now
+go get github.com/cespare/xxhash
+# TODO what is packages used for?
+go get golang.org/x/tools/go/packages
+go build github.com/dvyukov/go-fuzz/go-fuzz-build
+export GO_FUZZ_BUILD_PATH=`realpath go-fuzz-build`
+
+mkdir -p $GOPATH/src/github.com/protolambda
+
+# TODO why is eth2.0-specs in protolambda?
+cd $GOPATH/src/github.com/protolambda
+git clone https://github.com/ethereum/eth2.0-specs
+cd eth2.0-specs
+git checkout v0.6.0
+cd /eth2
+
+export GOPATH="$GOPATH:/eth2/lib/go:$ZRNT_GOPATH"
+
 cd /eth2/fuzzers
 # Recursively make all fuzzers
 make all

d0decf5407fd03531287c06dee73e1e563ec676f
gnattishness <1620192+gnattishness@users.noreply.github.com>
2019-10-21 16:50:28 +1100

    Lighthouse impl operational for attestation target.

--- files/fuzzers/attestation/fuzzer.cpp
+++ files/fuzzers/attestation/fuzzer.cpp
@@ -1,10 +1,34 @@
 #define GO_FUZZ_PREFIX attestation_
 #include <lib/differential.h>
 #include <lib/go.h>
+#include <lib/rust.h>
 #include <lib/ssz-preprocess.h>
 #include <cstring>
 
+extern "C" bool attestation_c(uint8_t* input_ptr, size_t input_size, uint8_t* output_ptr, size_t* output_size);
+
+namespace fuzzing {
+    class Lighthouse : public Rust {
+        std::optional<std::vector<uint8_t>> run(const std::vector<uint8_t>& _data) override {
+            /* Copy because attestation_c wants a non-const pointer */
+            std::vector<uint8_t> data(_data.data(), _data.data() + _data.size());
+
+            size_t output_size = data.size() * 4;
+            std::vector<uint8_t> ret(output_size);
+
+            if ( attestation_c(data.data(), data.size(), ret.data(), &output_size) == false ) {
+                return std::nullopt;
+            }
+
+            ret.resize(output_size);
+
+            return ret;
+        }
+    };
+} /* namespace fuzzing */
+
 std::shared_ptr<fuzzing::Go> go = nullptr;
+std::shared_ptr<fuzzing::Lighthouse> lighthouse = nullptr;
 
 std::unique_ptr<fuzzing::Differential> differential = nullptr;
 
@@ -13,6 +37,10 @@ extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
 
     differential->AddModule(
             go = std::make_shared<fuzzing::Go>()
+    );
+
+    differential->AddModule(
+            lighthouse = std::make_shared<fuzzing::Lighthouse>()
     );
 
     return 0;
---
+++ files/fuzzers/attestation/lighthouse/src/lib.rs
@@ -0,0 +1,78 @@
+// TODO doesn't this get shadowed?
+use ssz::{Decode, Encode};
+use ssz_derive::{Decode, Encode};
+use state_processing::{
+    per_block_processing::{process_attestations, VerifySignatures},
+    BlockProcessingError,
+};
+use std::{ptr, slice};
+use types::{Attestation, BeaconState, EthSpec, MainnetEthSpec};
+
+#[derive(Decode, Encode)]
+struct AttestationTestCase<T: EthSpec> {
+    pub pre: BeaconState<T>,
+    pub attestation: Attestation<T>,
+}
+
+impl<T: EthSpec> AttestationTestCase<T> {
+    /// Run `process_block_header` and return a `BeaconState` on success, or a
+    /// TODO change error
+    /// `BlockProcessingError` on failure.
+    fn process_attestation(mut self) -> Result<BeaconState<T>, BlockProcessingError> {
+        let spec = T::default_spec();
+
+        // TODO not certain whether we use beacon_node::beacon_chain::process_attestation,
+        // or eth2::state_processing::per_block_processing::process_attestations
+        // or possibly beacon_node:fork_choice
+        // I think process_attestations, but only due to existing types etc, not proper understanding
+        process_attestations(
+            &mut self.pre,
+            &[self.attestation],
+            VerifySignatures::True,
+            &spec,
+        )?;
+
+        Ok(self.pre)
+    }
+}
+
+/// Accepts an SSZ-encoded `AttestationTestCase` and returns an SSZ-encoded post-state on success,
+/// or nothing on failure.
+fn fuzz<T: EthSpec>(ssz_bytes: &[u8]) -> Result<Vec<u8>, ()> {
+    let test_case = match AttestationTestCase::from_ssz_bytes(&ssz_bytes) {
+        Ok(test_case) => test_case,
+        _ => return Err(()),
+    };
+
+    let post_state: BeaconState<T> = match test_case.process_attestation() {
+        Ok(state) => state,
+        _ => return Err(()),
+    };
+
+    Ok(post_state.as_ssz_bytes())
+}
+
+#[no_mangle]
+pub fn attestation_c(
+    input_ptr: *mut u8,
+    input_size: usize,
+    output_ptr: *mut u8,
+    output_size: *mut usize,
+) -> bool {
+    let input_bytes: &[u8] = unsafe { slice::from_raw_parts(input_ptr, input_size as usize) };
+
+    // Note: `MainnetEthSpec` contains the "constants" in the official spec.
+    if let Ok(output_bytes) = fuzz::<MainnetEthSpec>(input_bytes) {
+        unsafe {
+            if output_bytes.len() > *output_size {
+                return false;
+            }
+            ptr::copy_nonoverlapping(output_bytes.as_ptr(), output_ptr, output_bytes.len());
+            *output_size = output_bytes.len();
+        }
+
+        return true;
+    } else {
+        return false;
+    }
+}

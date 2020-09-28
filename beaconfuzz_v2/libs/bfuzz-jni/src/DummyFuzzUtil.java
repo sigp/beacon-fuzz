9571ff62fa10cf9d0eae2504b9d5ce44a634ebdf
Nathaniel Jensen <nathaniel@sigmaprime.io>
2020-09-28 12:58:19 +1000

    Dummy C and Java files to test components in isolation.

---
+++ beaconfuzz_v2/libs/bfuzz-jni/src/DummyFuzzUtil.java
@@ -0,0 +1,28 @@
+import java.util.Optional;
+
+public class DummyFuzzUtil {
+  // NOTE: alternatively could also have these all in separate classes, which implement a
+  // "FuzzHarness" interface
+
+  // Size of ValidatorIndex returned by shuffle
+  private static final int OUTPUT_INDEX_BYTES = Long.BYTES;
+
+  private final boolean disable_bls;
+
+  // NOTE: this uses primitive values as parameters to more easily call via JNI
+  public DummyFuzzUtil(final boolean useMainnetConfig, final boolean disable_bls) {
+    initialize(useMainnetConfig, disable_bls);
+    this.disable_bls = disable_bls;
+  }
+
+  public static void initialize(final boolean useMainnetConfig, final boolean disable_bls) {
+    System.out.println("Initializing in Java");
+  }
+
+  public Optional<byte[]> fuzzAttestation(final byte[] input) {
+    System.out.println("Calling Java fuzzAttestation!");
+    //Just return the input
+    //return Optional.of(input);
+    return Optional.empty();
+  }
+}
---
+++ beaconfuzz_v2/libs/bfuzz-jni/src/dummy.c
@@ -0,0 +1,18 @@
+#include "bfuzzjni.h"
+#include <stdio.h>
+
+int main() {
+  bfuzz_jni_init("DummyFuzzUtil", "fuzzAttestation", ".", 1);
+  uint8_t data[50] = {1,2,3,4,5};
+  int out = bfuzz_jni_run(data, 50);
+  printf("In C Result val=%d\n", out);
+  if (out > 0) {
+    uint8_t result[out];
+    bfuzz_jni_load_result(result, out);
+    printf("Result content: ");
+    for (int i = 0; i < 50; i++) {
+      printf("%d ", result[i]);
+    }
+    printf("\n");
+  }
+}
